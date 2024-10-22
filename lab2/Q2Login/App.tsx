d1acf30cc7b54783b7077c2611bc993999c9f8f8
HuynhMinhHai79 <huynhhai13979@gmail.com>
2024-10-22 19:36:33 +0700

    lab2_update
    update

---
+++ lab2/Q2Login/App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {Text, View} from 'react-native';
+import LoginScreen from './Login/Login';
+import React from 'react';
+function App(): React.JSX.Element {
+  return (
+    // <View>
+    //   <Text>adadad</Text>
+    // </View>
+    <LoginScreen />
+  );
+}
+
+export default App;
